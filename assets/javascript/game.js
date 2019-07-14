c6de463d749969e3c78f412d2b572ea9ac7610d2
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-07-14 00:01:32 -0500

    Meant to say checked against already guessed letters last time. Either way, just added an array of possible words and a function, which is called on page load, that chooses a random word out of that array and runs a for loop on it, adding each letter in turn to another array that contains the letters of the current word (in order) while at the same time adding an appropriate number of underscores (one for each letter) into the HTML under the h2 tag that says Current Word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     var currentWordLetters = [];
 
+    var possibleWords = ["bantha", "bowcaster", "carbonite", "droid", "ewok", "jedi", "landspeeder", "lightsaber", "nerfherder", "padawan", "sith", "wookie"];
+
+    function newWord() {
+        var wordChoice = Math.floor(Math.random() * possibleWords.length);
+        var nextWord = possibleWords[wordChoice];
+        possibleWords.splice(wordChoice, 1)
+
+        /*Use nextWord to populate the currentWordLetters array with its letters.
+
+        Do this by running a for loop and, for each letter, pushing it into
+        currentWordLetters and appending a _ beneath "Current Word" in the HTML.*/
+
+        for (i = 0; i < nextWord.length; i++) {
+            currentWordLetters.push(nextWord.charAt(i));
+            var letterSpace = document.createElement("p");
+            letterSpace.textContent = "_";
+            currentWord.appendChild(letterSpace);
+        }
+        
+        console.log(currentWordLetters);
+    };
+
+    newWord();
+
     document.addEventListener("keyup", function (event) {
         if (((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)) && (guessedLetters.length < 1)) {
             var keyPressed = document.createElement("p");
