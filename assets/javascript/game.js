d514ca63f662f9f23f4031c4c1fd742c2ed2f02c
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-07-13 22:54:22 -0500

    Made an if statement with a for loop that checks guessed words (on keyup) against those already guessed (which are kept in an array) and only adds them to the already guessed words in the HTML and array if they haven't already been guessed (multiple instances of .toLowerCase ensure that you can't guess the same letter twice by switching to/away from capital letters)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    var guessedLetters = [];
+
+    var currentWordLetters = [];
+
     document.addEventListener("keyup", function (event) {
-        if ((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)) {
+        if (((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)) && (guessedLetters.length < 1)) {
             var keyPressed = document.createElement("p");
-            keyPressed.textContent = event.key.toLowerCase();
-            //keyPressed.innerHTML = "<p style='text-align: left !important'; 'display: inline';>" + event.key.toLowerCase() + "</p>";
+            keyPressed.textContent = " " + event.key.toLowerCase();
             alreadyGuessed.appendChild(keyPressed);
-            console.log(keyPressed);
-        };
-    });
+            guessedLetters.push(event.key.toLowerCase());
+        }
+        else if (((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)) && (guessedLetters.length > 0)) {
+            for (i = 0; i < guessedLetters.length; i++) {
+                if (guessedLetters[i] === event.key.toLowerCase()) {
+                    break;
+                }
+                else if ((guessedLetters[i] !== event.key) && (i === (guessedLetters.length - 1))) {
+                    var keyPressed = document.createElement("p");
+                    keyPressed.textContent = ", " + event.key.toLowerCase();
+                    alreadyGuessed.appendChild(keyPressed);
+                    guessedLetters.push(event.key);
+                }
+            }
+        }
+    })
 });
