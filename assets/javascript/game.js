d247eb58c375e90796fdcc6973344895a369c0bd
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-07-13 19:25:19 -0500

    Added an if statement so that key presses that aren't alphabetical don't do anything

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.addEventListener("keyup", function(event) {
-        var keyPressed = event.key.toLowerCase();
-        console.log(keyPressed);
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.addEventListener("keyup", function (event) {
+        if ((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123)) {
+            var keyPressed = document.createElement("p");
+            keyPressed.textContent = event.key.toLowerCase();
+            //keyPressed.innerHTML = "<p style='text-align: left !important'; 'display: inline';>" + event.key.toLowerCase() + "</p>";
+            alreadyGuessed.appendChild(keyPressed);
+            console.log(keyPressed);
+        };
     });
 });
